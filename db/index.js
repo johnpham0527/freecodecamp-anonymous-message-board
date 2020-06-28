7ea52a004babb4a0a3b256bcb96746ac41ef48f4
John Pham <mr.john.pham@gmail.com>
2020-06-28 07:45:58 -0400

    Set up database connection

---
+++ db/index.js
@@ -0,0 +1,17 @@
+const MongoClient= require('mongodb').MongoClient;
+
+const CONNECTION_STRING = process.env.DB;
+
+function getDb() {
+  return MongoClient
+    .connect(CONNECTION_STRING, {useUnifiedTopology: true})
+    .then(client => {
+        console.log(`Database connected successfully.`);
+        return client.db('boards');
+    })
+    .catch(err => {
+      console.log('Database error: ' + err);
+  })
+}
+
+module.exports = getDb();
