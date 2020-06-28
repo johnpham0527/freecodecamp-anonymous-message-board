963334fb78df0f9acc25c5468958dfdebecfbb15
John Pham <mr.john.pham@gmail.com>
2020-06-28 08:34:59 -0400

    Set up getDb

--- controllers/threads.js
+++ controllers/threads.js
@@ -8,9 +8,16 @@ function postThreads(req, res, next) {
     const board = req.params.board;
     const { text, delete_password } = req.body;
     console.log(`POST threads. board is ${board}, text is ${text}, and delete_password is ${delete_password}`);
+
+
     /*
-    To implement: database functions
+    Saved will be at least _id, text, createdon_(date&time), bumpedon_(date&time, starts same as created_on), reported(boolean), deletepassword_, & replies(array).
     */
+    getDb.then(function(db) {
+        
+    });
+
+
     return res.send(`Hello world`);
     //Eventually, this will return a res redirect to /b/${req.params}
 }
