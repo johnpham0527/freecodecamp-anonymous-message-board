8afd9e3c68cf61985da1edd5adf341fe65be70c1
John Pham <mr.john.pham@gmail.com>
2020-06-28 07:37:49 -0400

    Add user story comments to functional tests

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -18,19 +18,37 @@ suite('Functional Tests', function() {
   suite('API ROUTING FOR /api/threads/:board', function() {
     
     suite('POST', function() {
+      /*
+      I can POST a thread to a specific message board by passing form data text and deletepassword_ to /api/threads/{board}.
       
+      (Recommend res.redirect to board page /b/{board}) 
+      
+      Saved will be at least _id, text, createdon_(date&time), bumpedon_(date&time, starts same as created_on), reported(boolean), deletepassword_, & replies(array).
+      */
+
     });
     
     suite('GET', function() {
+      /*
+      I can GET an array of the most recent 10 bumped threads on the board with only the most recent 3 replies each from /api/threads/{board}.
       
+      The reported and deletepasswords_ fields will not be sent to the client.
+      */
+
     });
     
     suite('DELETE', function() {
-      
+      /*
+      I can delete a thread completely if I send a DELETE request to /api/threads/{board} and pass along the threadid_ & deletepassword_. (Text response will be 'incorrect password' or 'success')
+      */
+
     });
     
     suite('PUT', function() {
-      
+      /*
+      I can report a thread and change its reported value to true by sending a PUT request to /api/threads/{board} and pass along the threadid_. (Text response will be 'success')
+      */
+
     });
     
 
@@ -39,19 +57,31 @@ suite('Functional Tests', function() {
   suite('API ROUTING FOR /api/replies/:board', function() {
     
     suite('POST', function() {
-      
+      /*
+       I can POST a reply to a thread on a specific board by passing form data text, deletepassword_, & threadid_ to /api/replies/{board} and it will also update the bumped_on date to the comments date.
+       
+       (Recommend res.redirect to thread page /b/{board}/{thread_id})
+       
+       In the thread's replies array will be saved _id, text, createdon_, deletepassword_, & reported.
+      */
     });
     
     suite('GET', function() {
-      
+      /*
+      I can GET an entire thread with all its replies from /api/replies/{board}?thread_id={thread_id}. Also hiding the same fields the client should be see.
+      */
     });
     
     suite('PUT', function() {
-      
+      /*
+      I can report a reply and change its reported value to true by sending a PUT request to /api/replies/{board} and pass along the threadid_ & replyid_. (Text response will be 'success')
+      */
     });
     
     suite('DELETE', function() {
-      
+      /*
+      I can delete a post(just changing the text to '[deleted]' instead of removing completely like a thread) if I send a DELETE request to /api/replies/{board} and pass along the threadid_, replyid_, & deletepassword_. (Text response will be 'incorrect password' or 'success')
+      */
     });
     
   });
