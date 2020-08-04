788394c4e62e5822ce118187915a632bd375e764
John Pham <mr.john.pham@gmail.com>
2020-08-04 09:24:30 -0400

    Debug: Error executing find and modify: MongoError: the update operation document must contain atomic operators.

--- controllers/threads.js
+++ controllers/threads.js
@@ -58,13 +58,20 @@ function postThreads(req, res) {
 }
 
 function putThreads(req, res, next) {
-const board = req.params.board;
-const { threadid_ } = req.query;
+    const board = req.params.board;
+    const { threadid_ } = req.query;
     /* I can report a thread and change its reported value to true by sending a PUT request to /api/threads/{board} and pass along the threadid_. (Text response will be 'success') */
 
-    const { threadid_ } = req.query;
+    console.log(`The thread id is ${threadid_}`);
     getDb.then(function(db) {
-    //db.collection(board).findAndModify({_id: ObjectId(threadid_)}, )
+        db.collection(board).findOneAndUpdate({ _id: ObjectId(threadid_)}, { reported: true }, { new: true },  function(err, result) {
+            if (err) {
+                console.log(`Error executing find and modify: ${err}`);
+            }
+
+            console.log(`result is ${result}`);
+            return res.json('success');
+        })
     });
 
 
